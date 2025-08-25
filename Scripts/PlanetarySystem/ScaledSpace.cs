24636b7d37d5e4776694d8dd40bef0b8848d52ab
andyduha1981@gmail.com <andyduha1981@gmail.com>
2025-08-25 15:05:35 +0600

    Add notes to ScaledSpace.cs because I WILL forget

--- Scripts/PlanetarySystem/ScaledSpace.cs
+++ Scripts/PlanetarySystem/ScaledSpace.cs
@@ -1,6 +1,14 @@
 using Godot;
 using System;
 
+/*
+Potentially add an "offset" position for when in map view so that scaled space can
+move around the camera rather than the camera moving around it 
+(also add override when in map view so that the camera can navigate around scaled space -
+currently all it is doing is moving in REAL space which causes floating point imprecision bugs)
+
+TL;DR the camera has to look at scaled space in map view, and have scaled space move instead of the camera.
+*/
 public partial class ScaledSpace : Node3D
 {
     // Obligatory.
