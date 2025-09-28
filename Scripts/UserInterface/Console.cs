92194e98a3c2ce44152846a9fa0948c51bfc01b3
Sushutt <95113669+Sushutt@users.noreply.github.com>
2025-09-27 17:32:25 -0700

    Window scaling

--- Scripts/UserInterface/Console.cs
+++ Scripts/UserInterface/Console.cs
@@ -3,8 +3,10 @@ using System;
 
 public partial class Console : RichTextLabel
 {
+    bool ctrlHeld = false;
     public override void _EnterTree()
     {
+        GuiInput += OnInput;
         Logger.OnLogged += AddLog;
         Text = "";
         Logger.Print("Ingame console ready");
@@ -19,5 +21,34 @@ public partial class Console : RichTextLabel
     public void AddLog(DateTime time, string content)
     {
         Text += $"[color=676767]{time:HH:mm:ss}[color=white]: {content}\n";
+    }
+
+    public override void _UnhandledInput(InputEvent inpEvent)
+    {
+        if (inpEvent is InputEventKey key)
+        {
+            if (key.Pressed && key.Keycode == Key.Ctrl)
+            {
+                ctrlHeld = true;
+            }else if (!key.Pressed && key.Keycode == Key.Ctrl)
+            {
+                ctrlHeld = false;
+            }
+        }
+    }
+
+    public void OnInput(InputEvent inpEvent)
+    {
+        if (inpEvent is InputEventMouseButton mouseMotion && ctrlHeld)
+        {
+            if (mouseMotion.ButtonIndex == MouseButton.WheelUp)
+            {
+                AddThemeFontSizeOverride("normal_font_size", GetThemeFontSize("normal_font_size") + 1);
+            }
+            if (mouseMotion.ButtonIndex == MouseButton.WheelDown)
+            {
+                AddThemeFontSizeOverride("normal_font_size", GetThemeFontSize("normal_font_size") - 1);
+            }
+        }
     }
 }
